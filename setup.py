4c8779491eb2710a456bae6fc6e629cfc0e6378b
AddressHunt <contact.addresshunt@gmail.com>
2022-04-23 18:35:07 +1000

    feat: make readme.rst to be description page on pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
 setup(
     name="addresshunt",
-    version='1.0',
+    version='1.1',
     packages=['addresshunt'],
     description='Python package for address validations',
     author='AddressHunt',
@@ -13,5 +18,7 @@ setup(
     ''',
     install_requires=[
     'requests'
-    ]
+    ],
+    long_description=long_description,
+    long_description_content_type='text/x-rst'
 )
